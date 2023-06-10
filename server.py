32ed5c742df70962ff099d8f20aedc226ac474ed
Denys Filichkin <denys.philichkin@gmail.com>
2023-06-10 17:04:12 +0300

    hw save

---
+++ server.py
@@ -0,0 +1,74 @@
+import asyncio
+import logging
+
+import aiohttp
+import websockets
+import names
+from websockets import WebSocketServerProtocol, WebSocketProtocolError
+from websockets.exceptions import ConnectionClosedOK
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def request(url):
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    r = await response.json()
+                    return r
+                logging.error(f"Error status {response.status} for {url}")
+        except aiohttp.ClientConnectionError as e:
+            logging.error(f"Connection error {url}: {e}")
+        return e
+
+
+async def get_exchange():
+    res = await request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+    exchange, *_ = list(filter(lambda el: el['ccy'] == 'USD', res))
+
+    return f"USD: buy: {exchange['buy']}, sale: {exchange['sale']}"
+
+
+class Server:
+    clients = set()
+
+    async def register(self, ws: WebSocketServerProtocol):
+        ws.name = names.get_full_name()
+        self.clients.add(ws)
+        logging.info(f'{ws.remote_address} connect')
+
+    async def unregister(self, ws: WebSocketServerProtocol):
+        self.clients.remove(ws)
+        logging.info(f'{ws.remote_address} disconnect')
+
+    async def send_to_clients(self, message: str):
+        if self.clients:
+            [await client.send(message) for client in self.clients]
+
+    async def ws_handler(self, ws: WebSocketServerProtocol):
+        await self.register(ws)
+        try:
+            await self.distrubute(ws)
+        except ConnectionClosedOK:
+            pass
+        finally:
+            await self.unregister(ws)
+
+    async def distrubute(self, ws: WebSocketServerProtocol):
+        async for message in ws:
+            if message == 'exchange':
+                r = await get_exchange()
+                await self.send_to_clients(r)
+            else:
+                await self.send_to_clients(f"{ws.name}: {message}")
+
+
+async def main():
+    server = Server()
+    async with websockets.serve(server.ws_handler, 'localhost', 8080):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
